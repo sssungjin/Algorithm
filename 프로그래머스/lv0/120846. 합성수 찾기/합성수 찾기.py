ee164b26e55aab7093affea973f631fc968ab2ca
Sungjin Cho <97083298+sssungjin@users.noreply.github.com>
2023-03-16 16:36:01 +0900

    [level 0] Title: 합성수 찾기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.py
@@ -0,0 +1,13 @@
+def solution(n):
+    answer = 0
+    num = 0
+    for i in range(4,n+1):
+        for j in range(1,i+1):
+            if i % j == 0:
+                num += 1
+            if num == 3:
+                answer += 1
+                num = 0
+                break
+            
+    return answer
