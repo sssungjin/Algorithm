3c53f77c11dda18c8fde7dfd26e52869334332dc
Sungjin Cho <97083298+sssungjin@users.noreply.github.com>
2023-03-17 12:50:48 +0900

    [unrated] Title: 기사단원의 무기, Time: 1.86 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.py
@@ -0,0 +1,16 @@
+def solution(number, limit, power):
+    answer = 0
+    attack = 0
+    lst = []
+    for i in range(1,number+1):
+        attack = 0
+        for j in range(1,int(i**(0.5))+1):
+            if i % j == 0:
+                attack += 1
+                if((j**2) != i):
+                    attack += 1
+        if attack > limit:
+            lst.append(power)
+        else:
+            lst.append(attack)
+    return sum(lst)
