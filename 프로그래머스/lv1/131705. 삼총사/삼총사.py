3b0ccc8a3e0ab2d2d61d707cab67d4517feabad9
Sungjin Cho <97083298+sssungjin@users.noreply.github.com>
2023-03-23 16:30:02 +0900

    [level 1] Title: 삼총사, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.py
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.py
@@ -1,8 +1,8 @@
+import itertools
+
 def solution(number):
     answer = 0
-    for i in range(len(number)):
-        for j in range(i+1,len(number)):
-            for k in range(j+1,len(number)):
-                if number[i] + number[j] + number[k] == 0:
-                    answer += 1
+    for i in itertools.combinations(number,3):
+        if sum(i) == 0:
+            answer += 1
     return answer
