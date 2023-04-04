a69560c0a7688092f05471a9fcdb6dfc52a5bada
Sungjin Cho <97083298+sssungjin@users.noreply.github.com>
2023-04-04 17:21:19 +0900

    [unrated] Title: 디펜스 게임, Time: 0.06 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.py
+++ 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.py
@@ -6,12 +6,12 @@ def solution(n, k, enemy):
     max_heap = []
     
     for i in enemy:
-        heapq.heappush(max_heap, -i)
+        heapq.heappush(max_heap, (-i, i))
         sum += i
         if sum > n:
             if k == 0:
                 break
-            sum += heapq.heappop(max_heap)
+            sum -= heapq.heappop(max_heap)[1]
             k -= 1
         answer += 1
     return answer
