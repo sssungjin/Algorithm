2e0c1d6b7bc75847a014f7b606f69728aaab633a
Sungjin Cho <97083298+sssungjin@users.noreply.github.com>
2023-06-27 14:13:56 +0900

    [unrated] Title: 콜라 문제, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
@@ -1,8 +1,6 @@
 def solution(a, b, n):
     answer = 0
     while(n >= a):
-        coke = n % a
-        n = n // a * b
-        answer += n
-        n += coke
+        answer += n // a * b
+        n = n // a * b + n % a
     return answer
