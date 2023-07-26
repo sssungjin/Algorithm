bd7125e2edada7413ec2745a2427847137dfd48e
Sungjin Cho <97083298+sssungjin@users.noreply.github.com>
2023-07-26 22:32:00 +0900

    [level 2] Title: 할인 행사, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
@@ -1,13 +1,14 @@
 def solution(want, number, discount):
     answer = 0
     for i in range(len(discount)-sum(number)+1):
+        flag = True
         s = discount[i:sum(number)+i]
         cnt = 0
         for w,n in zip(want, number):
             if s.count(w) >= n:
                 continue
             else:
-                cnt += 1
-        if cnt == 0: answer += 1
+                flag = False
+        if flag: answer += 1
     return answer
 
