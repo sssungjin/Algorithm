4730ab5001cbf97f8369c2420b3a3bb07502a26b
Sungjin Cho <97083298+sssungjin@users.noreply.github.com>
2024-01-28 18:08:10 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+SELECT b.TITLE, b.BOARD_ID, r.REPLY_ID, r.WRITER_ID, r.CONTENTS, DATE_FORMAT(r.CREATED_DATE, '%Y-%m-%d') AS CREATED_DATE
+from used_goods_board as b
+join used_goods_reply as r
+on r.board_id = b.board_id
+where YEAR(b.CREATED_DATE) = 2022 and MONTH(b.CREATED_DATE) = 10
+order by r.created_date, b.title;
+
