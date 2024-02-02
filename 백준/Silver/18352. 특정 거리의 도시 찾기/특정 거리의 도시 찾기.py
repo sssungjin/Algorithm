629e76f45044df81437a02c70083d5b9b9019a79
Sungjin Cho <97083298+sssungjin@users.noreply.github.com>
2024-02-02 21:53:18 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1236 ms, Memory: 158852 KB -BaekjoonHub

---
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+n, m, k, x = map(int, input().split())
+# 그래프 n+1 크기로 만들어서 1부터 시작하도록
+g = [[] for _ in range(n+1)]
+
+for i in range(m):
+    a, b = map(int, input().split())
+    g[a].append(b)
+
+distance = [-1] * (n+1)
+distance[x] = 0
+
+q = deque([x])
+while q:
+    start = q.popleft()
+
+    for nx in g[start]:
+        # 방문 안했으면
+        if distance[nx] == -1:
+            distance[nx] = distance[start] + 1
+            q.append(nx)
+tf = False
+
+for i in range(1, n + 1):
+    if distance[i] == k:
+        print(i)
+        tf=True
+if tf == False:
+    print(-1)
