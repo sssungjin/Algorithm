2228c00634b4e047772477f365ded7ee7fd04af3
Sungjin Cho <97083298+sssungjin@users.noreply.github.com>
2024-02-19 14:36:54 +0900

    [Silver II] Title: 병사 배치하기, Time: 744 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
@@ -0,0 +1,12 @@
+n = int(input())
+soldier = list(map(int, input().split()))
+
+cnt = 0
+dp = [1] * n
+for i in range(1, n):
+    for j in range(0, i):
+    # 내림차순이 맞으면
+        if soldier[j] > soldier[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(n - max(dp))
