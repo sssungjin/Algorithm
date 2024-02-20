8e73d91828a4ea95374fec88be214355b263de5a
Sungjin Cho <97083298+sssungjin@users.noreply.github.com>
2024-02-20 15:37:56 +0900

    [Silver I] Title: 스타트와 링크, Time: 5292 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,19 +1,29 @@
-from itertools import combinations, permutations
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-ability = [list(map(int, input().split())) for _ in range(n)]
-ans = 1e9
+graph = [list(map(int, input().split())) for _ in range(n)]
+visit = [False for _ in range(n)]
+min_value = 1e9
 
-for r1 in combinations(list(range(n)), n//2):
-    start, link = 0, 0
-    r2 = list(set(list(range(n))) - set(r1))
-    for r in combinations(r1, 2):
-        start += ability[r[0]][r[1]]
-        start += ability[r[1]][r[0]]
-    for r in combinations(r2, 2):
-        link += ability[r[0]][r[1]]
-        link += ability[r[1]][r[0]]
-    ans = min(ans, abs(start-link))
-print(ans)
+def dfs(depth, idx):
+    global min_value
+    if depth == n//2:
+        power1, power2 = 0, 0
+        for i in range(n):
+            for j in range(n):
+                # visit 돌면서 True 쌍을 찾아서 점수 더함
+                if visit[i] and visit[j]:
+                    power1 += graph[i][j]
+                # False 쌍을 찾아서 더함
+                elif not visit[i] and not visit[j]:
+                    power2 += graph[i][j]
+        min_value = min(min_value, abs(power1-power2))
+        return
+    for i in range(idx, n):
+        if not visit[i]:
+            visit[i] = True
+            dfs(depth+1, i+1)
+            visit[i] = False
+dfs(0, 0)
+print(min_value)
