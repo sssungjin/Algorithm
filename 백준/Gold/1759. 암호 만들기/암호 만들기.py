78ddc956d659a82a40fea84085bba27c91bcffdc
Sungjin Cho <97083298+sssungjin@users.noreply.github.com>
2024-02-22 17:39:44 +0900

    [Gold V] Title: 암호 만들기, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -0,0 +1,26 @@
+def back_tracking(cnt, idx):
+    if cnt == l:
+        # 모음, 자음 개수
+        vo, co = 0, 0
+
+        for i in range(l):
+            # 모음이라면
+            if answer[i] in consonant:
+                vo += 1
+            else:
+                co += 1
+        
+        if vo >= 1 and co >= 2:
+            print("".join(answer))
+        return
+    
+    for i in range(idx, c):
+        answer.append(words[i])
+        back_tracking(cnt +1, i + 1)
+        answer.pop()
+
+l, c = map(int, input().split())
+words = sorted(list(map(str, input().split())))
+consonant = ['a', 'e', 'i', 'o', 'u']
+answer = []
+back_tracking(0, 0)
