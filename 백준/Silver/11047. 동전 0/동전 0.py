4176ef2532f084257804f66807f0e6da821cc026
Sungjin Cho <97083298+sssungjin@users.noreply.github.com>
2024-02-28 18:25:54 +0900

    [Silver IV] Title: 동전 0, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,15 @@
+n, k = map(int, input().split())
+coins = []
+for i in range(n):
+    coins.append(int(input()))
+
+coins.sort(reverse=True)
+answer = 0
+
+while k != 0:
+    for coin in coins:
+        # k보다 작거나 같으면
+        if coin <= k:
+            answer += k // coin
+            k %= coin            
+print(answer)
