4a8fde8f22683d07ddff6b02440da623933dc8c4
Sungjin Cho <97083298+sssungjin@users.noreply.github.com>
2024-03-19 18:37:13 +0900

    [Silver III] Title: N과 M (3), Time: 1436 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+arr = []
+
+def solution():
+    if len(arr) == m:
+        print(' '.join(map(str, arr)))
+        return
+
+    for i in range(1, n+1):
+        arr.append(i)
+        solution()
+        arr.pop()
+
+solution()
