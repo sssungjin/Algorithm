8941c828f83a4a33ec028ab5ecae8fc2cc3073bf
Sungjin Cho <97083298+sssungjin@users.noreply.github.com>
2024-03-19 18:33:54 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -3,16 +3,11 @@ def dfs(start):
         print(' '.join(map(str, arr)))
 
     for i in range(start, n + 1):
-        if visited[i]:
-            continue
-        visited[i] = True
         arr.append(i)
         dfs(i + 1)
         arr.pop()
-        visited[i] = False
 
 n, m = map(int, input().split())
 arr = []
-visited = [False] * (n + 1)
 
 dfs(1)
