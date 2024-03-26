5e5a4b8078e032497d948437d3942a6c2d9dc50a
Sungjin Cho <97083298+sssungjin@users.noreply.github.com>
2024-03-26 18:13:36 +0900

    [level 2] Title: 더 맵게, Time: 1904.72 ms, Memory: 51.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,16 @@
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    
+    while scoville[0] < K:
+        a = heapq.heappop(scoville)
+        b = heapq.heappop(scoville)
+        heapq.heappush(scoville, a + b * 2)
+        answer += 1
+        
+        if len(scoville) == 1 and scoville[0] < K:
+            return -1
+    
+    return answer
