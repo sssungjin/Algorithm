e5d8c07eafb95c282c8233fa0ff45ccff8fd0a67
Sungjin Cho <97083298+sssungjin@users.noreply.github.com>
2025-02-07 17:26:42 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,25 +1,27 @@
 from collections import deque
+    
+dr = [-1, 0, 1, 0]
+dc = [0, 1, 0, -1]
 
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
-
-def bfs(x, y):
+def bfs(row, col):
     q = deque()
-    q.append((x, y))
-
+    q.append((row, col))
+    
     while q:
-        x, y = q.popleft()
+        r, c = q.popleft()
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 1:
-                q.append((nx, ny))
-                graph[nx][ny] = graph[x][y] + 1
-
+            nr = r + dr[i]
+            nc = c + dc[i]
+            if 0 <= nr < n and 0 <= nc < m and graph[nr][nc] == 1:
+                q.append((nr, nc))
+                graph[nr][nc] = graph[r][c] + 1
+                
+                
 n, m = map(int, input().split())
 graph = []
-for i in range(n):
-    graph.append(list(map(int, input())))
 
+for i in range(n):
+    graph.append(list(map(int , input())))
+    
 bfs(0, 0)
 print(graph[n-1][m-1])
