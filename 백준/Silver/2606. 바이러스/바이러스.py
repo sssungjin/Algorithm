380448e3ef109c0b9872d4742ec8f93f3174dcc9
Sungjin Cho <97083298+sssungjin@users.noreply.github.com>
2025-02-07 17:52:45 +0900

    [Silver III] Title: 바이러스, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,22 +1,20 @@
 n = int(input())
-num = int(input())
-graph = [[0] * (n+1) for _ in range(n+1)]
-visited = [False] * (n+1)
-# graph 연결 처리
-for i in range(num):
+m = int(input())
+graph = [[False] * (n + 1) for _ in range(n + 1)]
+visited = [False] * (n + 1)
+
+for i in range(m):
     x, y = map(int, input().split())
     graph[x][y] = 1
     graph[y][x] = 1
-
+    
 
 def dfs(v):
-    # 방문 = 감염
     visited[v] = True
     
-    for i in range(2, n+1):
-        # 방문하지 않았고 연결되어 있다면
+    for i in range(1, n + 1):
         if not visited[i] and graph[v][i] == 1:
             dfs(i)
-
+            
 dfs(1)
 print(visited.count(True) - 1)
