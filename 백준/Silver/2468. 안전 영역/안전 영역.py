0766c63c36d273a19d080c272cfd689f5acdab33
Sungjin Cho <97083298+sssungjin@users.noreply.github.com>
2025-02-16 19:49:01 +0900

    [Silver I] Title: 안전 영역, Time: 684 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,48 +1,40 @@
-import sys
 from collections import deque
-
-input = sys.stdin.readline
-
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
 
 n = int(input())
 graph = [list(map(int, input().split())) for _ in range(n)]
+visited = [[False] * n for _ in range(n)]
 
-# graph의 값이 height 이상이고 범위를 벗어나지 않는 경우 확인
-def bfs(height):
-    visited = [[False for _ in range(n)] for _ in range(n)]
-    ans = 0
-    q = deque()
+min_h = min(map(min, graph))
+max_h = max(map(max, graph))
+
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
+
+def bfs(r, c, h, visited):
+    q = deque([(r, c)])
+    visited[r][c] = True
+    while q:
+        r, c = q.popleft()
+        for i in range(4):
+            nr = r + dr[i]
+            nc = c + dc[i]
+            if 0 <= nr < n and 0 <= nc < n:
+                if not visited[nr][nc] and graph[nr][nc] > h:
+                    visited[nr][nc] = True
+                    q.append((nr, nc))
+                    
+max_safe_zone = 0
+
+for h in range(min_h - 1, max_h + 1):
+    visited = [[False] * n for _ in range(n)]
+    safe_zone_count = 0
+    
     for i in range(n):
         for j in range(n):
-            # 잠기지 않았고 방문하지 않았다면 큐에 추가 하고 
-            if graph[i][j] >= height and not visited[i][j]:
-                q.append((i, j))
-                while q:
-                    x, y = q.pop()
-                    for k in range(4):
-                        nx = dx[k] + x
-                        ny = dy[k] + y
-                        # 방문하지 않았고 잠기지 않았다면
-                        if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny] and graph[nx][ny] >= height:
-                            # 방문 처리
-                            visited[nx][ny] = True
-                            q.append((nx, ny))
-                # while문 끝나면 한 안전영역 구한 것
-                ans += 1
-    return ans
-
-def solution():
-    max_height = -1e9
-    for i in graph:
-        max_height = max(max_height, max(i))
-
-    safe_num = -1e9
-    for i in range(max_height+1):
-        safe_num = max(safe_num, bfs(i))
-    print(safe_num)
-
-solution()
-
-
+            if not visited[i][j] and graph[i][j] > h:
+                bfs(i, j, h, visited)
+                safe_zone_count += 1
+                
+    max_safe_zone = max(max_safe_zone, safe_zone_count)
+    
+print(max_safe_zone)
