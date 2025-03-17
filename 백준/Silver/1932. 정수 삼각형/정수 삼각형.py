b234f2e43968ebaf71b1e390cff5593cf9eb7bd4
Sungjin Cho <97083298+sssungjin@users.noreply.github.com>
2025-03-17 13:26:06 +0900

    [Silver I] Title: 정수 삼각형, Time: 152 ms, Memory: 36504 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,17 +1,14 @@
-tri = []
 n = int(input())
-for i in range(n):
-    tri.append(list(map(int, input().split())))
 
+triangle = [list(map(int, input().split())) for _ in range(n)]
 
 for i in range(1, n):
-     for j in range(len(tri[i])):
-        # 가장 왼쪽 또는 가장 오른쪽인지 확인
+    for j in range(len(triangle[i])):
         if j == 0:
-            tri[i][j] += tri[i-1][j]
-        elif j == (len(tri[i]) - 1):
-             tri[i][j] += tri[i-1][j-1]
+            triangle[i][j] += triangle[i-1][j]
+        elif j == len(triangle[i]) - 1:
+            triangle[i][j] += triangle[i-1][j-1]
         else:
-            tri[i][j] = max(tri[i][j] + tri[i-1][j-1], tri[i][j] + tri[i-1][j])
+            triangle[i][j] = max(triangle[i][j] + triangle[i-1][j-1], triangle[i][j] + triangle[i-1][j])
 
-print(max(tri[n-1]))
+print(max(triangle[n-1]))
