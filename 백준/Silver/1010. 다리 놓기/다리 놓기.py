76feb955ea9c7f1ba0e17bd46f74eabeb0f13765
Sungjin Cho <97083298+sssungjin@users.noreply.github.com>
2025-03-17 14:14:51 +0900

    [Silver V] Title: 다리 놓기, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -1,5 +1,8 @@
 t = int(input())
+
+bridge = [list(map(int, input().split())) for _ in range(t)]
 dp = [[0] * 30 for _ in range(30)]
+
 for i in range(30):
     for j in range(30):
         if i == 1:
@@ -10,6 +13,5 @@ for i in range(30):
             elif i < j:
                 dp[i][j] = dp[i-1][j-1] + dp[i][j-1]
 
-for _ in range(t):
-    n, m = list(map(int, (input().split())))
+for n, m in bridge:
     print(dp[n][m])
