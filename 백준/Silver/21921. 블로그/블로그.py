9c8849a11c9a5fa9b45f561bbea682db657989e8
Sungjin Cho <97083298+sssungjin@users.noreply.github.com>
2025-03-20 18:38:23 +0900

    [Silver III] Title: 블로그, Time: 180 ms, Memory: 58224 KB -BaekjoonHub

---
+++ 백준/Silver/21921. 블로그/블로그.py
@@ -0,0 +1,25 @@
+n, x = map(int, input().split())
+visitors = list(map(int, input().split()))
+window_size = n - x + 1
+
+window = [0] * window_size
+window[0] = sum(visitors[0:x])
+
+for i in range(1, window_size):
+    window[i] = window[i - 1] + visitors[i + x - 1] - visitors[i - 1]
+
+max_window = window[0]
+cnt = 1
+
+for i in range(1, window_size):
+    if window[i] > max_window:
+        max_window = window[i]
+        cnt = 1
+    elif window[i] == max_window:
+        cnt += 1
+
+if max_window == 0:
+    print("SAD")
+else:
+    print(max_window)
+    print(cnt)
