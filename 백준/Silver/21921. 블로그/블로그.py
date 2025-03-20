b4fe0b07a257c40d7e634bdded6040d1915ecfc5
Sungjin Cho <97083298+sssungjin@users.noreply.github.com>
2025-03-20 18:43:46 +0900

    [Silver III] Title: 블로그, Time: 176 ms, Memory: 58224 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.py
+++ 백준/Silver/21921. 블로그/블로그.py
@@ -4,14 +4,12 @@ window_size = n - x + 1
 
 window = [0] * window_size
 window[0] = sum(visitors[0:x])
-
-for i in range(1, window_size):
-    window[i] = window[i - 1] + visitors[i + x - 1] - visitors[i - 1]
-
 max_window = window[0]
 cnt = 1
 
 for i in range(1, window_size):
+    window[i] = window[i - 1] + visitors[i + x - 1] - visitors[i - 1]
+
     if window[i] > max_window:
         max_window = window[i]
         cnt = 1
