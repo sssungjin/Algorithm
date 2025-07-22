39ef27d99e01c3feb9b465f1484c57bded425867
Sungjin Cho <97083298+sssungjin@users.noreply.github.com>
2025-07-22 17:45:55 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -1,12 +1,12 @@
 n, k = map(int, input().split())
+queue = [i for i in range(1, n + 1)]
 idx = 0
-queue = list(range(1, n+1))
-res = []
+ans = []
 
 while queue:
     idx += k - 1
     if idx >= len(queue):
         idx %= len(queue)
-    res.append(str(queue.pop(idx)))
-
-print("<", ", ".join(res), ">", sep="")
+    ans.append(str(queue.pop(idx)))
+    
+print("<", ", ".join(ans), ">", sep = "")
