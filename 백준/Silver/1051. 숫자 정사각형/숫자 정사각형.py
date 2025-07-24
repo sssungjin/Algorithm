849c6e90d94906d2f27b6a4344963af36a3134af
Sungjin Cho <97083298+sssungjin@users.noreply.github.com>
2025-07-24 23:29:55 +0900

    [Silver III] Title: 숫자 정사각형, Time: 52 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+arr = []
+
+for i in range(n):
+    arr.append(list(map(int, input().strip())))
+ans = 0
+
+# width 는 정사각형 한 변의 길이
+for width in range(1, max(n, m)):
+    for i in range(n - width):
+        for j in range(m - width):
+            if arr[i][j] == arr[i + width][j] == arr[i + width][j + width] == arr[i][j + width]:
+                ans = max(ans, (width + 1) * (width + 1))
+            else:
+                continue
+
+if ans == 0:
+    ans = 1
+
+print(ans)
