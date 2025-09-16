b774a5c456e218b34048aec9d19de40c3b473f08
Sungjin Cho <97083298+sssungjin@users.noreply.github.com>
2025-09-16 21:15:56 +0900

    [level 3] Title: 야근 지수, Time: 331.22 ms, Memory: 9.42 MB -BaekjoonHub

---
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -0,0 +1,8 @@
+from heapq import heapify, heappush, heappop
+def solution(n, works):
+    heapify(works := [-i for i in works])
+    
+    for i in range(min(n, abs(sum(works)))):
+        heappush(works, heappop(works)+1)
+        
+    return sum([i*i for i in works])
