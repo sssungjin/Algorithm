eb8bbebde1ee537326d674fbc3efd58f15b9ef43
Sungjin Cho <97083298+sssungjin@users.noreply.github.com>
2025-10-13 19:56:23 +0900

    [level 2] Title: 구명보트, Time: 10.09 ms, Memory: 9.55 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/README.md
+++ 프로그래머스/2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885) 
 
 ### 성능 요약
 
-메모리: 10.5 MB, 시간: 8.82 ms
+메모리: 9.55 MB, 시간: 10.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 18일 17:40:29
+2025년 10월 13일 19:56:20
 
 ### 문제 설명
 
--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,17 +1,15 @@
 def solution(people, limit):
     answer = 0
     people.sort()
-    s, e = 0, len(people) - 1
+    start, end = 0, len(people) - 1
     
-    while s <= e:
-        # 두명 구출
-        if people[s] + people[e] <= limit:
-            s += 1
-            e -= 1
-        # 두명 안된다면 큰 사람 구출
+    while start <= end:
+        flag = True
+        if people[start] + people[end] <= limit:
+            start += 1
+            end -= 1
         else:
-            e -= 1
-        answer += 1
+            end -= 1
+        answer += 1        
+    
     return answer
-
-# 50 50 70 80
