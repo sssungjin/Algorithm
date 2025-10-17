c5d5f901d0e7de32f0fefc3a0c87106af26d0dd1
Sungjin Cho <97083298+sssungjin@users.noreply.github.com>
2025-10-17 19:59:01 +0900

    [Gold IV] Title: 치즈, Time: 80 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.py
+++ 백준/Gold/2636. 치즈/치즈.py
@@ -5,55 +5,59 @@ graph = [list(map(int, input().split())) for _ in range(N)]
 
 dr = [-1, 1, 0, 0]
 dc = [0, 0, -1, 1]
- 
+
 def bfs():
-    # visited 는 시간마다 초기화
+    
     visited = [[False] * M for _ in range(N)]
     queue = deque([(0, 0)])
+    
     visited[0][0] = True
-    melt_list = []
+    
+    melt_cheese = []
     
     while queue:
         r, c = queue.popleft()
         
         for i in range(4):
-            next_r, next_c = r + dr[i], c + dc[i]
+            nr, nc = r + dr[i], c + dc[i]
             
-            if 0 <= next_r < N and 0 <= next_c < M and not visited[next_r][next_c]:
-                # 공기를 만나면 탐색
-                if graph[next_r][next_c] == 0:
-                    visited[next_r][next_c] = True
-                    queue.append((next_r, next_c))
-                # 치즈를 만나면 녹을 치즈로 간주하고 melt_list에 추가
-                elif graph[next_r][next_c] == 1:
-                    visited[next_r][next_c] = True
-                    melt_list.append((next_r, next_c))
+            if 0 <= nr < N and 0 <= nc < M and not visited[nr][nc]:
+                
+                # 0이면 탐색
+                if graph[nr][nc] == 0:
+                    visited[nr][nc] = True
+                    queue.append((nr, nc))
+                if graph[nr][nc] == 1:
+                    visited[nr][nc] = True
+                    melt_cheese.append((nr, nc))
                     
-    return melt_list
+    return melt_cheese
 
-def count_cheese():
-    count = 0
+def calculate_cheese(graph):
+    cheese_cnt = 0
     for row in graph:
-        count += sum(row)
-    return count
-
-time = 0
-last_cheese_count = 0
+        cheese_cnt += sum(row)
+    
+    return cheese_cnt
+    
+    
+answer = 0
 
 while True:
-    current_cheese_count = count_cheese()
     
-    if current_cheese_count == 0:
+    current_cheese = calculate_cheese(graph)
+    
+    if current_cheese == 0:
         break
     
-    last_cheese_count = current_cheese_count
-    
+    last_cheese = current_cheese
+
     melt_list = bfs()
     
-    for r, c in melt_list:
-        graph[r][c] = 0
-        
-    time += 1
+    for a, b in melt_list:
+        graph[a][b] = 0
 
-print(time)
-print(last_cheese_count)
+    answer += 1
+
+print(answer)
+print(last_cheese)
